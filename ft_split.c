8e496845cbfddcf8072955ffc7028a1648f3952f
alvadiapedro <92808079+alvadiapedro@users.noreply.github.com>
2021-11-05 17:12:24 +0000

    Add files via upload

---
+++ ft_split.c
@@ -0,0 +1,20 @@
+#include "libft.h"
+
+int	word_count(const char *str, char c)
+{
+	size_t	i;
+	unsigned int	words;
+
+	i = 0;
+	words = 0;
+
+	while (str[i])
+	{
+		if (str[i] != c)
+	}
+}
+
+char	**ft_split(char const *s, char c)
+{
+	
+}
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -2,5 +2,16 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	
+	size_t	n;
+	char	*new;
+
+	n = ft_strlen(s1);
+	if (!s1 || !set)
+		return (NULL);
+	while (*s1 && ft_strchr(set, *s1))
+		s1++;
+	while (n && ft_strchr(set, s1[n]))
+		n--;
+	new = ft_substr(s1, 0, n + 1);
+	return (new);
 }
